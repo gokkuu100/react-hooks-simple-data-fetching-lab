fc3b1e044b7f14517808d91a478b14a08ee37003
goku <wkurts247@gmail.com>
2023-08-05 11:24:40 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
-// create your App component here
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [randomdog, setRandomDog] = useState('');
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((resp) => resp.json())
+      .then((data) => {
+        setRandomDog(data.message);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      {loading ? ( 
+        <p>Loading...</p>
+      ) : (
+        <img src={randomdog} alt='A Random Dog' />
+      )}
+    </div>
+  );
+}
+
+export default App;
